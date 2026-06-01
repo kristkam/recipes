22df9cd4277005b972ca64d7d28cd3d6512fabbf
k.kampenhoy <k.kampenhoy@sportradar.com>
2026-06-01 13:23:08 +0200

    Fix formatting in router configuration

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory('/recipes')
+  history: createWebHistory('/recipes'),
   routes: [
     {
       path: '/',
